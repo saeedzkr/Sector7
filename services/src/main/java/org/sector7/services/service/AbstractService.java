c92ddf3e88903b07909e0060c397266f5358e491
saeedzkr <s_zakipour@yahoo.com>
2015-12-29 22:06:05 +0330

    update

---
+++ services/src/main/java/org/sector7/services/service/AbstractService.java
@@ -0,0 +1,16 @@
+package org.service;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by sector7 on 12/10/15.
+ */
+public abstract class AbstractService {
+
+    Logger logger = Logger.getLogger("ServiceLogger");
+
+    public AbstractService()
+    {
+
+    }
+}
